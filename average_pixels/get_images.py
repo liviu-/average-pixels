c3b1fef64b3a383b017ec2e155cbdc5b58a6bf5c
liviu- <lp752@york.ac.uk>
2016-06-11 15:05:29 +0100

    Store files in /tmp/ and fetch API key from $HOME

--- average_pixels/get_images.py
+++ average_pixels/get_images.py
@@ -1,18 +1,16 @@
 import os
 import urllib
+import urllib.error
 import requests
 
-from api_key import API_KEY
-
-from IPython import embed as qq
 
 URL = "https://bingapis.azure-api.net/api/v5/images/search"
 NUMBER_OF_IMAGES = 10
-DIR = os.path.realpath('img')
+DIR = '/tmp/average_images'
 
-def search_images(term):
+def search_images(term, api_key):
     params = {"q": term, "count":NUMBER_OF_IMAGES}
-    headers = {'ocp-apim-subscription-key': API_KEY}
+    headers = {'ocp-apim-subscription-key': api_key}
     response = requests.request("GET", URL,
                                 headers=headers,
                                 params=params)
@@ -22,11 +20,30 @@ def search_images(term):
 def download_image(url, filename):
     urllib.request.urlretrieve(url, filename)
 
+def get_api_key():
+    try:
+        api_key_file = os.path.join(
+            os.path.expanduser('~'), ".average_pixels_api")
+        with open(api_key_file, 'r') as f:
+            api_key = f.read().replace('\n','')
+    except FileNotFoundError:
+        api_key = input("Please insert your API key: ")
+
+    return api_key
+
+
 def save_images(term):
-    images = search_images(term)
+    api_key = get_api_key()
+    images = search_images(term, api_key)
     filenames = []
 
+    if not os.path.exists(DIR):
+        os.makedirs(DIR)
+
+
     for i, img in enumerate(images):
+        if img['encodingFormat'] == 'unknown':
+            continue
         name = "{path}/{filename}.{ext}".format(
                 path=DIR,
                 filename="_".join(term.split()) + str(i),
